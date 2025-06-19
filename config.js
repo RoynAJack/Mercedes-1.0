cd2e261c6bf00878d67ad7cd1301c1d36f231657
RoynBosses09 <roynajack303@gmail.com>
2025-06-19 10:25:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -41,9 +41,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254740007567",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "263774918784",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Marisel",
+OWNER_NAME: process.env.OWNER_NAME || "Mabhosiedu RJ",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*Made By Marisel*",
 // add bot owner name    
